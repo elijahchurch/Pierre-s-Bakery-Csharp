e885d9de62c30bc41ddc51a0fc472607539c93cb
Jason E. Church <elijahchurch@gmail.com>
2023-07-14 10:21:20 -0700

    create TotalPastryCost field and pass test

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -1,16 +1,33 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Items.Models;
+using System;
 
 namespace Bakery.Tests
 {
     [TestClass]
-    public class PastryTests
+    public class PastryTests :IDisposable
     {
+        public void Dispose()
+        {
+            Pastry.TotalPastryCost = 0;
+        }
+
         [TestMethod]
         public void PastryCostField_CanAccessPastryCostField_Int()
         {
         int testPastryCost = Pastry.PastryCost;
-        Assert.AreEqual(2, testPastryCost);
+        Assert.AreEqual(2, testPastryCost);        
         }
+
+        [TestMethod]
+        public void TotalPastryCostField_CanAccessTotalPastryCost_Int()
+        {
+        int testTotalPastryCost = Pastry.TotalPastryCost;
+        Assert.AreEqual(0, testTotalPastryCost);
+        }
+
+
     }
+
+    
 }
--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -3,6 +3,6 @@ namespace Items.Models
     public class Pastry
     {
         static public int PastryCost {get;} = 2;
-        // Business Logic goes here.
+        static public int TotalPastryCost {get; set;}
     }
 }
