c7f50aeded84996e2aade04008553ff5070fe8b6
Jason E. Church <elijahchurch@gmail.com>
2023-07-14 13:53:16 -0700

    WIP: update Program.cs with refactored methods

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -12,8 +12,11 @@ namespace Bakery
             DisplayCosts();
             Console.WriteLine("If you are ready to make an order, enter any key.");
             Console.ReadLine();
-            Console.WriteLine("Great!");
-            CalculateOrder();
+            Console.WriteLine("Great! Let's get started.");
+            CalculateInitialOrder();
+            DisplayOrder();
+            Console.WriteLine("Thank you for ordering from Pierre's Bakery!");
+            Console.WriteLine("Have a good day!!!");
         }
 
         static void DisplayCosts()
@@ -22,8 +25,8 @@ namespace Bakery
             Console.WriteLine("Here are our prices:");
             Console.WriteLine("");
             Console.WriteLine("------------------------------------------------");
-            Console.WriteLine($"A loaf of bread costs: ${Bread.BreadCost}");
-            Console.WriteLine($"A single pastry costs: ${Pastry.PastryCost}");
+            Console.WriteLine($"A loaf of bread costs: ${Bread.BreadCost}.");
+            Console.WriteLine($"A single pastry costs: ${Pastry.PastryCost}.");
             Console.WriteLine(" ");
             Console.WriteLine("DAILY DEAL: Buy 2 loaves of bread get 1 free!");
             Console.WriteLine("DAILY DEAL: Buy 3 pastries get 1 free!");
@@ -31,7 +34,7 @@ namespace Bakery
             Console.WriteLine("");
         }
 
-        static void CalculateOrder()
+        static void CalculateInitialOrder()
         {
             Console.WriteLine("How many loaves of bread do you want to order?");
             string breadStringAmount = Console.ReadLine();
@@ -41,18 +44,67 @@ namespace Bakery
             {
                 int breadAmount = int.Parse(breadStringAmount);
                 int pastryAmount = int.Parse(pastryStringAmount);
+                Bread.CalculateBreadOrder(breadAmount);
+                Pastry.CalculatePastryOrder(pastryAmount);
+                Console.WriteLine("Here is your current order:");
+                DisplayOrder();
+                CheckIfOrderIsComplete();
             }
             catch
             {
-                Console.WriteLine("");
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("ERROR: Please only enter in numbers!");
-                Console.WriteLine("Special symbols and alphabetic characters will not be accepted.");
-                Console.ResetColor();
-                Console.WriteLine("Enter any key to try again.");
-                Console.ReadLine();
-                CalculateOrder();
+                ErrorMessage();
+                CalculateInitialOrder();
             }
+        }
+
+        static void DisplayOrder()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine($"Number of loaves of bread ordered: {Bread.AmountOfLoaves}.");
+            Console.WriteLine($"Number of pastries ordered: {Pastry.AmountOfPastries}.");
+            int totalCost = Bread.TotalBreadCost + Pastry.TotalPastryCost;
+            Console.WriteLine($"The cost of your order: ${totalCost}.");
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("");
+        }
+
+        static void ErrorMessage()
+        {
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("ERROR: Please only enter in numbers!");
+            Console.WriteLine("Special symbols and alphabetic characters will not be accepted.");
+            Console.ResetColor();
+            Console.WriteLine("Enter any key to try again.");
+            Console.ReadLine();
+        }
+
+        static void CheckIfOrderIsComplete()
+        {
+            Console.WriteLine("Would you like to add more to your order?");
+            Console.WriteLine("Enter 'yes' if you would like to add more loaves or pastries.");
+            Console.WriteLine("Otherwise enter 'no' to continue to checkout.");
+            string userResponse = Console.ReadLine().ToLower();
+            if (userResponse == "yes" || userResponse == "y")
+                {
+
+                }
+            else if ( userResponse == "no" || userResponse == "n")
+                {
+                    Console.WriteLine("Here is your final order:")
+                }
+            else 
+                {
+                    Console.WriteLine("");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("ERROR: Please only enter 'yes' or 'no'.");
+                    Console.ResetColor();
+                    Console.WriteLine("Enter any key to try again.");
+                    Console.ReadLine();
+                    Console.WriteLine("");
+                    CheckIfOrderIsComplete();
+                }
         }
     }
 }
