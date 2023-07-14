8fc65ec5c0a3b9ad76941b5dc324db50b5596118
Jason E. Church <elijahchurch@gmail.com>
2023-07-14 09:02:59 -0700

    create TotalBreadOrder static field and pass test to access it

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -1,16 +1,29 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Items.Models;
+using System;
 
 namespace Bakery.Tests
 {
     [TestClass]
-    public class BreadTests
+    public class BreadTests : IDisposable    
     {
+        public void Dispose()
+        {
+            Bread.TotalBreadOrder = 0;
+        }
+
         [TestMethod]
         public void BreadCostField_CanAccessBreadCostField_Int()
         {
         int testBreadCost = Bread.BreadCost;
         Assert.AreEqual(5, testBreadCost);
+        }
+
+        [TestMethod]
+        public void TotalBreadOrderField_CanAccessTotalBreadOrder_Int()
+        {
+        int testTotalBreadOrder = Bread.TotalBreadOrder;
+        Assert.AreEqual(0, testTotalBreadOrder);
         }
     }
 }
