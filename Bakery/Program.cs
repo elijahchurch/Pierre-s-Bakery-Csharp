f800fa1b0468cc67a9f8bc8401139a4ccc824d52
Jason E. Church <elijahchurch@gmail.com>
2023-07-14 11:05:05 -0700

    set up base UI logic in program.cs

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,4 +1,5 @@
 using Items.Models;
+using System;
 
 namespace Bakery
 {
@@ -6,7 +7,22 @@ namespace Bakery
     {
         static void Main()
         {
-        // UI code goes here.
+            Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~");
+            Console.WriteLine("Welcome to Pierre's Bakery!");
+            Console.WriteLine("Here are our prices:");
+            Console.WriteLine("");
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine($"A loaf of bread costs: ${Bread.BreadCost}");
+            Console.WriteLine($"A single pastry costs: ${Pastry.PastryCost}");
+            Console.WriteLine(" ");
+            Console.WriteLine("DAILY DEAL: Buy 2 loaves of bread get 1 free!");
+            Console.WriteLine("DAILY DEAL: Buy 3 pastries get 1 free!");
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("");
+            Console.WriteLine("If you are ready to make an order, enter any key.");
+            Console.ReadLine();
+            Console.WriteLine("Great!");
+
         }
     }
 }
